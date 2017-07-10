ec62f4177dfec5dc76cb9448c1b5deaf2db692b5
Sebastian Walter <sebastian.walter1@allianz.de>
2017-07-10 13:17:14 +0200

    sdf

---
+++ src/main/java/start/spring/io/greetings/controller/Greetings.java
@@ -0,0 +1,21 @@
+package start.spring.io.greetings.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by user on 10.07.17.
+ */
+
+@RestController
+public class Greetings {
+    public Greetings() {
+
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Juhu!";
+    }
+
+}
